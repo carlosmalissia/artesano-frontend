0340b1c9da275114ba1af94b6f5e304dde87f683
Carlos Malissia <116571296+geniototal@users.noreply.github.com>
2024-08-01 14:09:04 -0300

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -48,14 +48,7 @@ export default function Card({ props }) {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-row flex-nowrap justify-around items-center bg-cream rounded-b-xl p-1">
-                        <p className="bg-dark-aqua text-xs text-center text-brown rounded-full pl-1 pr-1">
-                            3 cuotas sin interes
-                        </p>
-                        <button className="bg-dark-aqua text-brown text-bold rounded-full pr-2 pl-2">
-                            +
-                        </button>
-                    </div>
+                    
                 </div>
             </div>
 
